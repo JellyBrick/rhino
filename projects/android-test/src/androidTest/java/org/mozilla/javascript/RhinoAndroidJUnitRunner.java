aec644fc0a1c574cddbb39abce83bf6a96937caf
seven332 <seven332@163.com>
2019-01-29 15:12:02 +0800

    Add android test

---
+++ projects/android-test/src/androidTest/java/org/mozilla/javascript/RhinoAndroidJUnitRunner.java
@@ -0,0 +1,23 @@
+package org.mozilla.javascript;
+
+import android.support.test.runner.AndroidJUnitRunner;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class RhinoAndroidJUnitRunner extends AndroidJUnitRunner {
+
+  @Override
+  public void onStart() {
+    System.setProperty("mozilla.js.tests", "testsrc/tests");
+    System.setProperty("mozilla.js.tests.timeout", "180000");
+    System.setProperty("user.language", "en");
+    System.setProperty("user.country", "US");
+    System.setProperty("user.timezone", "America/Los_Angeles");
+    System.setProperty("file.encoding", "UTF-8");
+
+    TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
+    Locale.setDefault(Locale.US);
+
+    super.onStart();
+  }
+}
---
+++ toolsrc/org/mozilla/javascript/tools/AndroidFileProvider.java
@@ -0,0 +1,113 @@
+package org.mozilla.javascript.tools;
+
+import android.support.test.InstrumentationRegistry;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+public class AndroidFileProvider extends FileProvider {
+
+  private static final String LOG_TAG = "AndroidTestResources";
+
+  private final File tempDir = new File(InstrumentationRegistry.getContext().getCacheDir(), "temp");
+  private final File assetsDir = new File(InstrumentationRegistry.getContext().getFilesDir(), "testassets");
+  private final File assetsName = new File(InstrumentationRegistry.getContext().getFilesDir(), "testassets.sha1");
+
+  private boolean assetsChecked = false;
+
+  public AndroidFileProvider() {
+    try {
+      FileUtils.forceMkdir(tempDir);
+      FileUtils.cleanDirectory(tempDir);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static boolean equals(Object a, Object b) {
+    return (a == b) || (a != null && a.equals(b));
+  }
+
+  private static String fixFilename(String path) {
+    if (path.startsWith("testsrc")) {
+      path = path.substring("testsrc/".length());
+    } else if (path.startsWith("test262")) {
+      path = path.substring("test262/".length());
+    }
+    return path;
+  }
+
+  private void ensureAssetFiles() throws IOException, ZipException {
+    String except;
+    try {
+      except = FileUtils.readFileToString(assetsName, "UTF-8");
+    } catch (IOException e) {
+      except = null;
+    }
+
+    String actual = null;
+    String[] assets = InstrumentationRegistry.getContext().getAssets().list("");
+    for (String asset : assets) {
+      if (asset.startsWith("testassets-") && asset.endsWith(".crc32")) {
+        actual = asset;
+      }
+    }
+    if (actual == null) {
+      throw new RuntimeException("Can't find test asset. Here is all assets: " + Arrays.toString(assets));
+    }
+
+    if (!equals(except, actual)) {
+      Log.d(LOG_TAG, "Copy test assets");
+
+      FileUtils.forceMkdir(assetsDir);
+      FileUtils.cleanDirectory(assetsDir);
+
+      try (InputStream in = InstrumentationRegistry.getContext().getAssets().open("testassets.zip");
+          OutputStream out = new FileOutputStream(new File(tempDir, "testassets.zip")) ) {
+        IOUtils.copy(in, out);
+      }
+
+      ZipFile zipFile = new ZipFile(new File(tempDir, "testassets.zip"));
+      zipFile.extractAll(assetsDir.getPath());
+
+      FileUtils.writeStringToFile(assetsName, actual, "UTF-8");
+    } else {
+      Log.d(LOG_TAG, "Test assets UP-TO-DATE");
+    }
+  }
+
+  @Override
+  public File getFile(String path) {
+    try {
+      if (!assetsChecked) {
+        synchronized (this) {
+          if (!assetsChecked) {
+            assetsChecked = true;
+            ensureAssetFiles();
+          }
+        }
+      }
+    } catch (IOException | ZipException e) {
+      throw new RuntimeException(e);
+    }
+
+    if (path.isEmpty()) {
+      return assetsDir;
+    }
+    path = fixFilename(path);
+
+    if (path.startsWith("/")) {
+      return new File(path);
+    } else {
+      return new File(assetsDir, path);
+    }
+  }
+}
